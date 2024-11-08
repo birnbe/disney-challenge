07aa438e89c958c76687ab95602a9a0c90dadb87
birnbe <dr.zaius@gmail.com>
2024-11-08 16:00:33 -0500

    search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-extra-boolean-cast */
 import SearchHeader from "./components/SearchHeader";
 import CharacterCard from "./components/CharacterCard";
 import FeaturedCharacters from "./components/FeaturedCharacters";
@@ -6,26 +7,48 @@ import { useDisneyCharacter } from "./contexts/DisneyCharacterContext";
 import { CharacterType } from "./types";
 
 function App() {
-  const { defaultDisney } = useDisneyCharacter();
+  const { visibleCharacters, searchString, defaultDisney } =
+    useDisneyCharacter();
+
+  const isDefaultLoaded = defaultDisney.some((char) => char !== undefined);
+
+  const isInitialDefaultLoad =
+    isDefaultLoaded &&
+    visibleCharacters.length === 0 &&
+    searchString.length > 2;
 
   return (
     <>
       <SearchHeader />
       <main>
         <section className="bg-disney-blue-100 p-5 md:p-10 xl:p-20 gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 place-items-center">
-          {defaultDisney.map((char: CharacterType | undefined) => {
-            if (!char) return null;
+          {searchString.length > 2 ? (
+            <h2 className="text-4xl mb-10 sm:col-span-2 md:col-span-3 xl:col-span-4 text-center">
+              Search Results - {searchString}
+            </h2>
+          ) : null}
 
-            return (
-              <CharacterCard
-                key={char._id}
-                id={char._id}
-                imageUrl={char.imageUrl}
-                name={char.name}
-                films={char.films}
-              />
-            );
-          })}
+          {visibleCharacters.length > 0 ? (
+            visibleCharacters.map((char: CharacterType | undefined) => {
+              if (!char) return null;
+
+              return (
+                <CharacterCard
+                  key={char._id}
+                  id={char._id}
+                  imageUrl={char.imageUrl}
+                  name={char.name}
+                  films={char.films}
+                />
+              );
+            })
+          ) : (
+            <h4 className="text-base sm:col-span-2 md:col-span-3 xl:col-span-4 text-center">
+              {isInitialDefaultLoad
+                ? "Sorry, your search has no results."
+                : "Loading Disney Characters..."}
+            </h4>
+          )}
         </section>
         <FeaturedCharacters />
       </main>
--- src/contexts/DisneyCharacterContext.tsx
+++ src/contexts/DisneyCharacterContext.tsx
@@ -5,10 +5,12 @@ import React, {
   ReactNode,
   useEffect,
 } from "react";
-import { CharacterType } from "../types";
-import { GetSpecificCharacters } from "../utils/api";
+import { useQuery, useQueries } from "@tanstack/react-query";
+import axios from "axios";
+import { CharacterResponse, CharacterType } from "../types";
 import { compact } from "lodash";
 
+const DISNEYAPI = "https://api.disneyapi.dev/character";
 const DISNEY_DEFAULT_IDS = [3347, 3389, 156, 25, 2099, 256, 3771, 4994];
 const FEATURED_IDS = [571, 544, 4703, 1947];
 
@@ -18,8 +20,11 @@ interface DisneyCharacterContextType {
   addCharacter: (character: CharacterType) => void;
   addCharacters: (newCharacters: CharacterType[]) => void;
   getCharacterById: (id: number) => CharacterType | undefined;
+  setSearchString: (nameStr: string) => void; //CharacterType | CharacterType[] | undefined;
   defaultDisney: (CharacterType | undefined)[];
   featuredDisney: (CharacterType | undefined)[];
+  visibleCharacters: (CharacterType | undefined)[];
+  searchString: string;
 }
 
 // Initialize the context with empty values
@@ -28,21 +33,68 @@ const DisneyCharacterContext = createContext<DisneyCharacterContextType>({
   addCharacter: () => {},
   addCharacters: () => {},
   getCharacterById: () => undefined,
+  setSearchString: () => undefined,
   defaultDisney: [],
   featuredDisney: [],
+  visibleCharacters: [],
+  searchString: "",
 });
+
+const fetchCharData = async (
+  charId: number
+): Promise<CharacterResponse<CharacterType>> =>
+  axios.get(`${DISNEYAPI}/${charId}`).then((res) => res.data);
+
+const GetSpecificCharacters = (chars: number[] | number) => {
+  const characterArray = Array.isArray(chars) ? chars : [chars];
+  return useQueries({
+    queries: characterArray.map((charId) => ({
+      queryKey: ["character", charId],
+      queryFn: () => fetchCharData(charId),
+    })),
+    combine: (results) => {
+      return {
+        data: results.map((result) => result.data?.data),
+        pending: results.some((result) => result.isPending),
+      };
+    },
+  });
+};
 
 // Define the Provider component
 export const DisneyCharacterProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const [characters, setCharacters] = useState<CharacterType[]>([]);
+  const [visibleCharacters, setVisibleCharacters] = useState<
+    (CharacterType | undefined)[]
+  >([]);
   const [defaultDisney, setDefaultDisney] = useState<
     (CharacterType | undefined)[]
   >([]);
   const [featuredDisney, setFeaturedDisney] = useState<
     (CharacterType | undefined)[]
   >([]);
+
+  const [searchString, setSearchStringState] = useState("");
+
+  const SearchForCharactersByName = useQuery<
+    CharacterResponse<CharacterType[]>
+  >({
+    queryKey: ["character-name", searchString],
+    enabled: searchString.length > 2,
+    queryFn: async () => {
+      const response = await fetch(
+        `${DISNEYAPI}?name=${encodeURIComponent(searchString)}`
+      );
+      if (!response.ok) {
+        throw new Error("Character response was not ok");
+      }
+      return await response.json();
+    },
+  });
+
+  const { data: charactersByNameData } = SearchForCharactersByName;
 
   // Function to add a new character, checking for duplicates by _id
   const addCharacter = (newCharacter: CharacterType) => {
@@ -61,13 +113,9 @@ export const DisneyCharacterProvider: React.FC<{ children: ReactNode }> = ({
   // Function to add an array of new characters, checking for duplicates by _id
   const addCharacters = (newCharacters: CharacterType[]) => {
     setCharacters((prevCharacters: CharacterType[]) => {
-      // const isDuplicate = prevCharacters.some(
-      //   (character) => character._id === newCharacter._id
-      // );
-
       // Filter out items that already exist in prevItems based on id
       const uniqueCharacters = newCharacters.filter(
-        newChar => !prevCharacters.some(item => item._id === newChar._id)
+        (newChar) => !prevCharacters.some((item) => item._id === newChar._id)
       );
 
       // Add only the unique items to the previous items
@@ -80,20 +128,37 @@ export const DisneyCharacterProvider: React.FC<{ children: ReactNode }> = ({
     return characters.find((character) => character._id === id);
   };
 
+  const setSearchString = (nameStr: string) => {
+    setSearchStringState(nameStr);
+  };
+
+  useEffect(() => {
+    if (searchString.length <= 2 && searchString.length >= 0) {
+      setVisibleCharacters(defaultDisney);
+    } else if (charactersByNameData?.data) {
+      const nameData = Array.isArray(charactersByNameData?.data)
+        ? charactersByNameData?.data
+        : [charactersByNameData?.data];
+      addCharacters(compact(nameData));
+      setVisibleCharacters(nameData);
+    }
+  }, [searchString, charactersByNameData?.data]);
+
   const defaultDisneyData = GetSpecificCharacters(DISNEY_DEFAULT_IDS);
   const featuredDisneyData = GetSpecificCharacters(FEATURED_IDS);
 
   // initialize default and featured list of characters
   useEffect(() => {
-      setDefaultDisney(defaultDisneyData.data);
-      if (!defaultDisneyData.pending) {
+    setDefaultDisney(defaultDisneyData.data);
+    if (!defaultDisneyData.pending) {
       addCharacters(compact(defaultDisneyData.data));
+      setVisibleCharacters(compact(defaultDisneyData.data));
     }
   }, [defaultDisneyData]);
 
   useEffect(() => {
-      setFeaturedDisney(featuredDisneyData.data);
-      if (!featuredDisneyData.pending) {
+    setFeaturedDisney(featuredDisneyData.data);
+    if (!featuredDisneyData.pending) {
       addCharacters(compact(featuredDisneyData.data));
     }
   }, [featuredDisneyData]);
@@ -106,8 +171,11 @@ export const DisneyCharacterProvider: React.FC<{ children: ReactNode }> = ({
         addCharacter,
         addCharacters,
         getCharacterById,
+        setSearchString,
         defaultDisney,
         featuredDisney,
+        visibleCharacters,
+        searchString,
       }}
     >
       {children}
